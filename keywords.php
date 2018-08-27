826a66cc79267fd1c704ffd74671971c39fab6c9
Unknown <jrady721@gmail.com>
2018-08-27 12:29:50 +0900

    init

---
+++ keywords.php
@@ -0,0 +1,25 @@
+<?php
+// include simple_html_dom
+include 'include/simplehtmldom_1_5/simple_html_dom.php';
+
+// getKeywords Top 20
+function getKeywords()
+{
+    header('Content-Type: application/json');
+
+    $html = file_get_html('http://www.naver.com');
+
+    $keyword = array("status" => '200');
+    $list = array();
+
+    foreach ($html->find('.ah_k') as $index => $element) {
+        if ($index == 20) break;
+        $list[$index + 1] = $element->plaintext;
+    }
+    $keyword['list'] = $list;
+
+    return json_encode($keyword, JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT);
+}
+
+// getKeywords call
+echo getKeywords();
