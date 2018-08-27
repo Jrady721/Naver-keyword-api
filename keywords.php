eba6f14590ec549346079a8b798a231ca6cb8745
Unknown <jrady721@gmail.com>
2018-08-27 16:11:57 +0900

    -

--- keywords.php
+++ keywords.php
@@ -7,17 +7,19 @@ function getKeywords()
 {
     header('Content-Type: application/json');
 
+    // parsing
     $html = file_get_html('http://www.naver.com');
 
     $keyword = array("status" => '200');
-    $list = array();
 
+    $list = array();
     foreach ($html->find('.ah_k') as $index => $element) {
         if ($index == 20) break;
         $list[$index + 1] = $element->plaintext;
     }
     $keyword['list'] = $list;
 
+    // return json
     return json_encode($keyword, JSON_UNESCAPED_UNICODE + JSON_PRETTY_PRINT);
 }
 
